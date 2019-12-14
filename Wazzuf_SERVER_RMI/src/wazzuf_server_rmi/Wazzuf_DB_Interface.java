bc7ffcfa0b814f2e920ac44dc27f541eca652869
Ahmed Abohegazy <35717011+Ahmed-Abohegazy@users.noreply.github.com>
2019-12-15 00:01:30 +0200

    Added throws RemoteException;
    Added throws RemoteException;

--- Wazzuf_SERVER_RMI/src/wazzuf_server_rmi/Wazzuf_DB_Interface.java
+++ Wazzuf_SERVER_RMI/src/wazzuf_server_rmi/Wazzuf_DB_Interface.java
@@ -17,22 +17,22 @@ import server_Client_Classes.JobSeeker;
  */
 public interface Wazzuf_DB_Interface extends Remote{
     //INSERT JOB SEEEKER
-    public void insertJobseeker(JobSeeker s);
+    public void insertJobseeker(JobSeeker s) throws RemoteException;;
     // GET JOB SEEKER BY USER NAME AND PASSWORD
-    public JobSeeker getJobSeeker(String Username, String Password);
+    public JobSeeker getJobSeeker(String Username, String Password)throws RemoteException;;
     //GET COMPANY BY  USER NAME AND PASSWORD
-    public Company getCompany(String Username, String Password);
+    public Company getCompany(String Username, String Password)throws RemoteException;;
     
-    public void insertCompany(Company s);
-    public void insertJob(Job j);
-    public void insertApplication(Application A);
-    public void deleteJobSeeker(int JSid );
-    public void deleteCompany(int compID );
-    public void deleteJob(int JID );
-    public void deleteApplication(int AID );
-    public JobSeeker getJobSeekerByID(int id );
-    public Company getCompanyByID(int id );
-     public Job getJobByID(int id );
-     public Application getApplicationByID(int id );
+    public void insertCompany(Company s)throws RemoteException;;
+    public void insertJob(Job j)throws RemoteException;;
+    public void insertApplication(Application A)throws RemoteException;;
+    public void deleteJobSeeker(int JSid )throws RemoteException;;
+    public void deleteCompany(int compID )throws RemoteException;;
+    public void deleteJob(int JID )throws RemoteException;;
+    public void deleteApplication(int AID )throws RemoteException;;
+    public JobSeeker getJobSeekerByID(int id )throws RemoteException;;
+    public Company getCompanyByID(int id )throws RemoteException;;
+     public Job getJobByID(int id )throws RemoteException;;
+     public Application getApplicationByID(int id )throws RemoteException;;
         
 }
